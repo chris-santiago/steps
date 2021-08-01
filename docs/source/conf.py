45466f454890533045277dcaa74b325120ae7bf7
Chris Santiago <chrissantiago@257-MBA.local>
2021-07-31 21:58:57 -0400

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,22 @@ extensions = [
     'myst_parser',
 ]
 
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
